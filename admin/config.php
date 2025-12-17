9bd560dcef9a5ae5869f92a285ef12f091a7a042
youssraalali <youssraalali4@gmail.com>
2025-12-17 18:35:03 +0200

    Initial project upload

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ishtari_test_learn/admin/');
+define('HTTP_CATALOG', 'http://localhost/ishtari_test_learn/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ishtari_test_learn/admin/');
+define('HTTPS_CATALOG', 'http://localhost/ishtari_test_learn/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\ishtari_test_learn/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\ishtari_test_learn/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\ishtari_test_learn/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\ishtari_test_learn/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\ishtari_test_learn/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\ishtari_test_learn/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\ishtari_test_learn/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\ishtari_test_learn/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\ishtari_test_learn/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\ishtari_test_learn/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\ishtari_test_learn/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_PORT', '3306');
+define('DB_DATABASE', 'ishtari_test_learn_db');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ishtari_test_learn/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ishtari_test_learn/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\ishtari_test_learn/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\ishtari_test_learn/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\ishtari_test_learn/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\ishtari_test_learn/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\ishtari_test_learn/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\ishtari_test_learn/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\ishtari_test_learn/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\ishtari_test_learn/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\ishtari_test_learn/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\ishtari_test_learn/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ishtari_test_learn_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+?>
